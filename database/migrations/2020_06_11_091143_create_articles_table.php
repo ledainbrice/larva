fb04545bd7f834a77ac2e1476f38a10ecc7790b4
Imoca <mickan007@gmail.com>
2020-06-11 15:32:31 +0200

    init

---
+++ database/migrations/2020_06_11_091143_create_articles_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('body');
+            $table->json('tags');
+            $table->timestamps();
+        });
+    }
+}
